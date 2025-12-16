f455b650016d9e8aa74fa59f4c0a9d86d4c5f7dc
Bannadadara <bannada.dara@gmail.com>
2025-12-16 18:01:30 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Services from "./sections/Services";
+import Products from "./sections/Products";
+import Note from "./sections/Note";
+import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Products />
+      <Note />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
