84f22c4408e803d99167ade4e7d937884c32db76
Bannadadara <bannada.dara@gmail.com>
2025-12-16 19:15:38 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: "center", padding: "16px", background: "#222", color: "#fff" }}>
-      © 2024 Bannada Daara
-    </footer>
-  );
-}
--- src/sections/Note.jsx
+++
@@ -1,8 +0,0 @@
-export default function Note() {
-  return (
-    <section style={{ padding: "20px", background: "#fff3cd" }}>
-      Designs may vary. Catalogue images are for reference only.
-      Customisation available.
-    </section>
-  );
-}
