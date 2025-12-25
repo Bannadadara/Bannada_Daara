cf499c998972a19b0d85fef50116535ce929ee3e
Bannadadara <bannada.dara@gmail.com>
2025-12-25 22:02:05 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+// This file acts as your product database
+export const products = [
+    // BAGS
+    { id: 1, name: "Reversible Tote Bag", price: 500, category: "Bags", img: "images/reversible-tote1.jpg" },
+    { id: 2, name: "Box Tote (Plain)", price: 350, category: "Bags", img: "images/box-plain.jpg" },
+    { id: 3, name: "Box Tote (Patch-work)", price: 500, category: "Bags", img: "images/box-patch.jpg" },
+    { id: 4, name: "String Sling Bag", price: 150, category: "Bags", img: "images/sling-reg.jpg" },
+    { id: 5, name: "Regular Sling Bag", price: 250, category: "Bags", img: "images/sling-reg-m.jpg" },
+    { id: 6, name: "Medium Sling Bag", price: 450, category: "Bags", img: "images/sling-reg.jpg" },
+    { id: 7, name: "Sling Bag (Patch-work S)", price: 250, category: "Bags", img: "images/sling-patch-s.jpg" },
+    { id: 8, name: "Sling Bag (Patch-work L)", price: 550, category: "Bags", img: "images/sling-patch-l.jpg" },
+    { id: 9, name: "Laptop Bag (50-50)", price: 800, category: "Bags", img: "images/pouch.jpg" }, 
+    { id: 10, name: "Potli", price: 250, category: "Bags", img: "images/potli.jpg" },
+    { id: 11, name: "Foldable Grocery Bag", price: 250, category: "Bags", img: "images/grocery.jpg" },
+
+    // POUCHES
+    { id: 12, name: "U-Shape Pouch (S)", price: 100, category: "Pouches", img: "images/u-pouch.jpg" },
+    { id: 13, name: "Travel Kit", price: 170, category: "Pouches", img: "images/travel-kit.jpg" },
+    { id: 14, name: "Pad-Holder", price: 100, category: "Pouches", img: "images/pad-holder-folded.jpg" },
+    { id: 15, name: "Flat Pouch", price: 80, category: "Pouches", img: "images/flat-pouch.jpg" },
+    { id: 16, name: "Box Pouch", price: 170, category: "Pouches", img: "images/box-pouch.jpg" },
+    { id: 17, name: "Trinket", price: 40, category: "Pouches", img: "images/TRINKET.jpg" },
+
+    // STATIONERY
+    { id: 18, name: "A4 Files", price: 200, category: "Stationery", img: "images/files.jpg" },
+    { id: 19, name: "Pen Pouch", price: 100, category: "Stationery", img: "images/pouch.jpg" },
+    { id: 20, name: "Book (Embroidered Cover)", price: 450, category: "Stationery", img: "images/book.jpg" },
+
+    // ACCESSORIES & DECOR
+    { id: 21, name: "Cutlery Kit", price: 260, category: "Accessories", img: "images/cutlery.jpg" },
+    { id: 22, name: "Mask", price: 50, category: "Accessories", img: "images/mask.jpg" },
+    { id: 23, name: "Patch-work Quilt", price: 0, category: "Decor", img: "images/quilt.jpg", on_request: true },
+    { id: 24, name: "Patch-work Table Cloth", price: 0, category: "Decor", img: "images/tcloth.jpg", on_request: true }
+];
